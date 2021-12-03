1816d431178d2f0b1f78b970e451305008a1cf6c
Caio <csbc326@gmail.com>
2021-12-03 18:08:18 -0300

    :rocket: Change docker compose

--- Projeto/apps/apps/settings.py
+++ Projeto/apps/apps/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['nginx-pcs3643-c4i0kun.cloud.okteto.net',
                  'nginx-pcs3643-berbardo.cloud.okteto.net',
-                 'nginx-blabla-berbardo.cloud.okteto.net',
-                 'nginx-testar-berbardo.cloud.okteto.net']
+                 'nginx-blabla2-berbardo.cloud.okteto.net',
+                 'nginx-testando-berbardo.cloud.okteto.net']
 
 
 # Application definition
